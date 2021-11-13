062728ae21fca649cfda9655a279e3a9b07a3e34
Warawit Siriyong <warawit.j@gmail.com>
2021-11-13 16:42:53 +0700

    finish commit

---
+++ src/components/ImageData.js
@@ -0,0 +1,25 @@
+const ImageData = [
+    {
+        title:"ภูเขา",
+        image:'https://cdn.pixabay.com/photo/2021/10/23/03/35/mountain-6734031_960_720.jpg'
+    },
+    {
+        title:"เกาะแมวน้ำ",
+        image:'https://cdn.pixabay.com/photo/2021/09/15/15/48/seals-6627197_960_720.jpg'
+    },
+    {
+        title:"ทะเล",
+        image:'https://cdn.pixabay.com/photo/2021/09/24/02/48/sea-6651168_960_720.jpg'
+    },
+    {
+        title:"น้ำตก",
+        image:'https://cdn.pixabay.com/photo/2020/04/28/12/54/iceland-5104370_960_720.jpg'
+    },
+    {
+        title:"ป่าในฤดูใบไม้ร่วง",
+        image:'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_960_720.jpg'
+    }
+
+]
+
+export default ImageData
---
+++ src/components/ImageSlider.js
@@ -0,0 +1,37 @@
+import ImageData from "./ImageData"
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai"
+import { useState } from "react"
+
+const ImageSlider = ()=>{
+    const [current,setCurrent] = useState(0)
+    const length = ImageData.length
+
+    const prevSlide=()=>{
+        current === 0 ? setCurrent(length-1) : setCurrent(current-1)
+    }
+    const nextSlide=()=>{
+        current === length-1 ? setCurrent(0) : setCurrent(current+1)
+    }
+    return(
+        <section className="slider">
+            <AiOutlineArrowLeft className="leftArrow" onClick={prevSlide}/>
+            <AiOutlineArrowRight className="rightArrow" onClick={nextSlide}/>
+            {ImageData.map((data,index)=>{
+                return(
+                    <div className={index === current ? "slide active" : "slide"} key={index}>
+                        <div>
+                            {index === current && (
+                                <div>
+                                    <img src={data.image} alt={data.title} className="image" />
+                                    <p>{data.title}</p>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                )
+            })}
+        </section>
+    )
+}
+
+export default ImageSlider
